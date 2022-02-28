dfa8dd19bfc475ef816c0899b7f3a6ccb8969e1e
Kristaps Baumanis <kristaps8894@gmail.com>
2022-02-28 12:33:22 +0200

    Viss strada tika nevar pievienoties ALB. Kkada problema.

--- main.tf
+++ main.tf
@@ -3,19 +3,25 @@ resource "aws_iam_role" "iam_for_lambda" {
 
   assume_role_policy = <<EOF
 {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "lambda.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
 }
 EOF
+}
+
+resource "aws_iam_role_policy" "nuTevJastrada" {
+  name = "nuTevJastradaBlin"
+  role = aws_iam_role.iam_for_lambda.id
+  policy = file("ec2_policy.json")
 }
 # resource "aws_lambda_layer_version" "lambda_layer" {
 #   filename   = "python.zip"

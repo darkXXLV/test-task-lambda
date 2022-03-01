4361349eada59cfb95c35542689bcdf96851ba9d
Kristaps Baumanis <kristaps8894@gmail.com>
2022-03-01 15:09:17 +0200

    WORKS

--- vpc.tf
+++ vpc.tf
@@ -34,6 +34,23 @@ resource "aws_internet_gateway" "gateway" {
   }
 }
 
+resource "aws_default_route_table" "route_table" {
+  default_route_table_id = aws_vpc.main.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway.id
+  }
+
+  tags = {
+    Name = "routing_table"
+  }
+}
+
+resource "aws_main_route_table_association" "route_table_a" {
+  vpc_id         = aws_vpc.main.id
+  route_table_id = aws_default_route_table.route_table.id
+}
 # resource "aws_network_acl" "main" {
 #   vpc_id = aws_vpc.main.id
 #   default = true
